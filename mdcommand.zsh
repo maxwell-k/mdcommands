b74636f8d21872516dad5b7fa101bcc7381074a2
Keith Maxwell <keith.maxwell@gmail.com>
2025-04-14 08:37:22 +0100

    feat: add more information to the abort prompt

--- mdcommand.zsh
+++ mdcommand.zsh
@@ -7,7 +7,7 @@ mdcommand() {
   local block="$(command mdcommands $@ | fzf --layout=reverse)"
 
   printf '%% %s\n' "$block"
-  read -s -q "REPLY?Abort? "
+  read -s -q "REPLY?Press y to abort or any other key to continue."
   if [ "$REPLY" = "n" ] ; then
     print -S "$block"
     printf '\n'
