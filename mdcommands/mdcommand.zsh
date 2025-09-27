7ca43d020a50310093711cb0774f813544f24fe7
Keith Maxwell <keith.maxwell@gmail.com>
2025-09-27 16:08:10 +0100

    feat: switch to a ZLE widget
    For a more intuitive interface; drop the press to continue or abort
    prompt.

--- mdcommands/mdcommand.zsh
+++ mdcommands/mdcommand.zsh
@@ -3,21 +3,9 @@
 # Copyright 2024 Keith Maxwell
 [ -n "${ZSH_VERSION:-}" ] || return 0
 
-mdcommand() {
-  local block="$(command mdcommands $@ | fzf --layout=reverse)"
-  if [ -z "$block" ] ; then
-    printf '\nInterrupted.\n'
-    return 1
-  fi
-
-  printf '%% %s\n' "$block"
-  read -s -k "REPLY?Press [Space] to continue or any other key to abort."
-  if [ "$REPLY" != " " ] ; then
-    printf '\nAborted.\n'
-    return 1
-  fi
-
-  print -S "$block"
-  printf '\n'
-  eval "$block"
+mdcommand-widget() {
+  LBUFFER="$(command mdcommands $LBUFFER | fzf --layout=reverse)"
+  zle reset-prompt
 }
+zle -N mdcommand-widget
+bindkey -M viins '^N' mdcommand-widget
